9acc612034771bb4fea194dda9253abb9cb02082
blackhaird <1652233944@qq.com>
2022-11-27 19:38:11 +0800

    修复数据库bug

--- common/database.go
+++ common/database.go
@@ -3,17 +3,24 @@ package common
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/database_name?charset=utf8")
+var db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/catering?charset=utf8")
 
-func initDB() {
+func InitDB() {
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("database connect success")
 	}
 	return
 }
 
-func getDB() *sql.DB {
+func GetDB() *sql.DB {
 	return db
+}
+
+func GetError() error {
+	return err
 }
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"gin_catering_backEnd/common"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func main() {
+	common.InitDB()
 	gin_server := gin.Default()
-
 	gin_server = CollectRoute(gin_server)
 	err := gin_server.Run(":8401")
 	if err != nil {
