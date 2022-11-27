126678328acd009f29c6c0341efbc7aa5207337c
blackhaird <1652233944@qq.com>
2022-11-27 18:23:39 +0800

    新增连接数据库

--- common/database.go
+++ common/database.go
@@ -1,1 +1,19 @@
 package common
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/database_name?charset=utf8")
+
+func initDB() {
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
+func getDB() *sql.DB {
+	return db
+}
